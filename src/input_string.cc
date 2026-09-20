1518b123747897f2d65d994bed6ca0380aebac08
Javier Acosta Portocarrero <alu0101660760@ull.edu.es>
2026-09-20 13:31:01 +0100

    First implementation of the methods of the input string class, with validations to avoid narrowing access to memory or incorrect behaviour

---
+++ src/input_string.cc
@@ -0,0 +1,50 @@
+// University of La Laguna
+// School of Engineering and Technology
+// Bachelor's Degree in Computer Engineering
+// Course: Computational Complexity
+// Year: 4th
+// Assignment 1: Pushdown Automaton
+// Author: Javier Acosta Portocarrero
+// Date: 20/09/2026
+// File input_string.cc: implementation file.
+// Contains the implementation of the InputString class.
+
+#include "../include/input_string.h"
+
+/**
+ * @brief Returns the current input symbol.
+ *
+ * @return The current symbol, or '.' if the input string is empty (epsilon equivalent).
+ */
+Symbol InputString::GetCurrentSymbol() const {
+  // Im using '.' as the equivalent to epsilon in a classic pushdown automaton.
+  return inner_string_ == nullptr? '.' : (*inner_string_)[current_position_];
+}
+
+/**
+ * @brief Advances to the next symbol of the input string.
+ */
+void InputString::AdvanceString() {
+  if (inner_string_ != nullptr && current_position_ < inner_string_->length()) {
+    ++current_position_;
+  }
+}
+
+/**
+ * @brief Checks if the input string has been completely processed.
+ *
+ * @return true if the input string is empty or fully processed or fale if not.
+ */
+bool InputString::IsEmpty() const {
+  return inner_string_ == nullptr || current_position_ >= inner_string_->length();
+}
+
+/**
+ * @brief Sets a new input word.
+ *
+ * @param input_word New input word.
+ */
+void InputString::IntroduceNewInputWord(const std::string& input_word) {
+  inner_string_ = &input_word;
+  current_position_ = 0;
+}
