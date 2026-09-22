118bced48fd201a3d9f53afd2ecb649e861544d6
Javier Acosta Portocarrero <alu0101660760@ull.edu.es>
2026-09-23 00:46:22 +0100

    Provitional project classes structure

---
+++ src/transition.cc
@@ -0,0 +1,16 @@
+// University of La Laguna
+// School of Engineering and Technology
+// Bachelor's Degree in Computer Engineering
+// Course: Computational Complexity
+// Year: 4th
+// Assignment 1: Pushdown Automaton
+// Author: Javier Acosta Portocarrero
+// Date: 20/09/2026
+// File transition.cc: implementation file.
+// Contains the implementation of the Transition class.
+
+#include "../include/transition.h"
+
+bool Transition::operator<(const Transition& transition_to_compare) const {
+  
+}
